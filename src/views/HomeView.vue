425c43bf0774ab2859b1053dca031ae0d7d75594
晓龙 <280751922@qq.com>
2025-11-24 22:36:23 +0800

    feat: 添加Swiper组件并优化页面布局
    - 在HomeView中添加Swiper轮播组件
    - 移除App.vue中多余的div包裹
    - 更新index.html添加全屏样式
    - 添加swiper和sass-embedded依赖

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,29 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1 class="text-3xl">欢迎来到首页</h1>
+  <Swiper class="w-screen h-screen" @swiper="onSwiper" @slideChange="onSlideChange">
+    <SwiperSlide>Slide 1</SwiperSlide>
+    <SwiperSlide>Slide 2</SwiperSlide>
+    <SwiperSlide>Slide 3</SwiperSlide>
+    <SwiperSlide>Slide 4</SwiperSlide>
+    <SwiperSlide>Slide 5</SwiperSlide>
+    <SwiperSlide>Slide 6</SwiperSlide>
+  </Swiper>
 </template>
 
-<style lang="scss" scoped></style>
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from "swiper/vue";
+import type { Swiper as SwiperType } from "swiper";
+
+import "swiper/css";
+import { ref } from "vue";
+
+const swiperRef = ref<SwiperType | null>(null);
+
+const onSwiper = (swiper: SwiperType) => {
+  swiperRef.value = swiper;
+  console.log(swiper);
+};
+const onSlideChange = () => {
+  console.log('slide change');
+};
+
+</script>
