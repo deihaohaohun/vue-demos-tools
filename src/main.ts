adf67f092bc0bd8e5ee89fbe3b39a8acf613a94d
晓龙 <280751922@qq.com>
2025-11-25 15:06:18 +0800

    feat: 添加PrimeVue和Dock组件，优化首页Swiper展示

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,19 @@ import router from './router'
 
 import './app.css'
 
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import Tooltip from 'primevue/tooltip'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
+app.directive('tooltip', Tooltip)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,47 @@
 <template>
   <Swiper class="w-screen h-screen" @swiper="onSwiper" @slideChange="onSlideChange">
-    <SwiperSlide>Slide 1</SwiperSlide>
-    <SwiperSlide>Slide 2</SwiperSlide>
-    <SwiperSlide>Slide 3</SwiperSlide>
-    <SwiperSlide>Slide 4</SwiperSlide>
-    <SwiperSlide>Slide 5</SwiperSlide>
-    <SwiperSlide>Slide 6</SwiperSlide>
+    <SwiperSlide v-for="nmu in 6" :key="nmu">
+      <div
+        style="height: 100%; background-image: url(https://primefaces.org/cdn/primevue/images/dock/window.jpg); background-size: cover;">
+      </div>
+    </SwiperSlide>
   </Swiper>
+
+  <Dock class="mb-4" :model="items" position="bottom">
+    <template #itemicon="{ item }">
+      <img v-tooltip.top="item.label" :src="item.icon" style="width: 100%" />
+    </template>
+  </Dock>
+
 </template>
 
 <script setup lang="ts">
 import { Swiper, SwiperSlide } from "swiper/vue";
 import type { Swiper as SwiperType } from "swiper";
+import { Dock } from "primevue";
 
 import "swiper/css";
 import { ref } from "vue";
 
 const swiperRef = ref<SwiperType | null>(null);
+const items = ref([
+  {
+    label: 'Finder',
+    icon: 'https://primefaces.org/cdn/primevue/images/dock/finder.svg'
+  },
+  {
+    label: 'App Store',
+    icon: 'https://primefaces.org/cdn/primevue/images/dock/appstore.svg'
+  },
+  {
+    label: 'Photos',
+    icon: 'https://primefaces.org/cdn/primevue/images/dock/photos.svg'
+  },
+  {
+    label: 'Trash',
+    icon: 'https://primefaces.org/cdn/primevue/images/dock/trash.png'
+  }
+]);
 
 const onSwiper = (swiper: SwiperType) => {
   swiperRef.value = swiper;
@@ -27,3 +52,5 @@ const onSlideChange = () => {
 };
 
 </script>
+
+<style lang="scss" scoped></style>
