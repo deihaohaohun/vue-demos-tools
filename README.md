53098082d5eaca7b8b5fa1450f411a98fc0d3dbb
晓龙 <280751922@qq.com>
2025-11-25 22:29:51 +0800

    feat: 更新README和依赖，优化HomeView组件的Swiper展示

--- README.md
+++ README.md
@@ -1,48 +1,15 @@
-# crud-app
+# Vue3 Demo Codes
 
-This template should help get you started developing with Vue 3 in Vite.
+Vite, Vue3 练手项目
 
-## Recommended IDE Setup
+## 计划实现的功能
 
-[VS Code](https://code.visualstudio.com/) + [Vue (Official)](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+- 收藏网址
+- 收藏图片
+- 收藏文本
 
-## Recommended Browser Setup
+## 使用到的技术
 
-- Chromium-based browsers (Chrome, Edge, Brave, etc.):
-  - [Vue.js devtools](https://chromewebstore.google.com/detail/vuejs-devtools/nhdogjmejiglipccpnnnanhbledajbpd) 
-  - [Turn on Custom Object Formatter in Chrome DevTools](http://bit.ly/object-formatters)
-- Firefox:
-  - [Vue.js devtools](https://addons.mozilla.org/en-US/firefox/addon/vue-js-devtools/)
-  - [Turn on Custom Object Formatter in Firefox DevTools](https://fxdx.dev/firefox-devtools-custom-object-formatters/)
-
-## Type Support for `.vue` Imports in TS
-
-TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) to make the TypeScript language service aware of `.vue` types.
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vite.dev/config/).
-
-## Project Setup
-
-```sh
-pnpm install
-```
-
-### Compile and Hot-Reload for Development
-
-```sh
-pnpm dev
-```
-
-### Type-Check, Compile and Minify for Production
-
-```sh
-pnpm build
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-pnpm lint
-```
+- PrimeVue
+- Swiper
+- Supabase
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,32 @@
   <Swiper class="w-screen h-screen" @swiper="onSwiper" @slideChange="onSlideChange">
     <SwiperSlide v-for="nmu in 6" :key="nmu">
       <div
-        style="height: 100%; background-image: url(https://primefaces.org/cdn/primevue/images/dock/window.jpg); background-size: cover;">
+        style="
+          height: 100%;
+          background-image: url(https://wowtabextension.z7.web.core.windows.net/wowtabwallpapers/2025-03-01.jpg);
+          background-size: cover;
+        "
+        class="flex justify-center items-center"
+      >
+        <div class="flex flex-wrap w-[80vw] gap-8">
+          <div
+            v-for="star in 30"
+            :key="star"
+            class="star w-32 h-32 flex flex-col items-center cursor-pointer"
+          >
+            <div
+              class="icon w-26 h-26 rounded-2xl backdrop-filter backdrop-blur-xl hover:scale-110 transition-all ease-in duration-200"
+            ></div>
+            <div class="title text-neutral-100 text-center">star</div>
+          </div>
+          <div class="star w-32 h-32 flex flex-col items-center">
+            <div
+              class="icon w-26 h-26 rounded-2xl backdrop-filter backdrop-blur-xl flex items-center justify-center text-3xl cursor-pointer"
+            >
+              +
+            </div>
+          </div>
+        </div>
       </div>
     </SwiperSlide>
   </Swiper>
@@ -12,45 +37,43 @@
       <img v-tooltip.top="item.label" :src="item.icon" style="width: 100%" />
     </template>
   </Dock>
-
 </template>
 
 <script setup lang="ts">
-import { Swiper, SwiperSlide } from "swiper/vue";
-import type { Swiper as SwiperType } from "swiper";
-import { Dock } from "primevue";
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import type { Swiper as SwiperType } from 'swiper'
+import { Dock } from 'primevue'
 
-import "swiper/css";
-import { ref } from "vue";
+import 'swiper/css'
+import { ref } from 'vue'
 
-const swiperRef = ref<SwiperType | null>(null);
+const swiperRef = ref<SwiperType | null>(null)
 const items = ref([
   {
     label: 'Finder',
-    icon: 'https://primefaces.org/cdn/primevue/images/dock/finder.svg'
+    icon: 'https://primefaces.org/cdn/primevue/images/dock/finder.svg',
   },
   {
     label: 'App Store',
-    icon: 'https://primefaces.org/cdn/primevue/images/dock/appstore.svg'
+    icon: 'https://primefaces.org/cdn/primevue/images/dock/appstore.svg',
   },
   {
     label: 'Photos',
-    icon: 'https://primefaces.org/cdn/primevue/images/dock/photos.svg'
+    icon: 'https://primefaces.org/cdn/primevue/images/dock/photos.svg',
   },
   {
     label: 'Trash',
-    icon: 'https://primefaces.org/cdn/primevue/images/dock/trash.png'
-  }
-]);
+    icon: 'https://primefaces.org/cdn/primevue/images/dock/trash.png',
+  },
+])
 
 const onSwiper = (swiper: SwiperType) => {
-  swiperRef.value = swiper;
-  console.log(swiper);
-};
+  swiperRef.value = swiper
+  console.log(swiper)
+}
 const onSlideChange = () => {
-  console.log('slide change');
-};
-
+  console.log('slide change')
+}
 </script>
 
 <style lang="scss" scoped></style>
