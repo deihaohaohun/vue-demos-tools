39977e34ee306cdcaf0de488866b1d977d5e6b14
晓龙 <280751922@qq.com>
2025-11-27 19:55:50 +0800

    feat: 实现翻页功能

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,20 @@
 <template>
   <Swiper class="w-screen h-screen" @swiper="onSwiper" @slideChange="onSlideChange">
     <SwiperSlide v-for="nmu in 6" :key="nmu">
-      <div
-        style="
+      <div style="
           height: 100%;
           background-image: url(https://wowtabextension.z7.web.core.windows.net/wowtabwallpapers/2025-03-01.jpg);
-          background-size: cover;
-        "
-        class="flex justify-center items-center"
-      >
+        " class="flex justify-center items-center">
         <div class="flex flex-wrap w-[80vw] gap-8">
-          <div
-            v-for="star in 30"
-            :key="star"
-            class="star w-32 h-32 flex flex-col items-center cursor-pointer"
-          >
+          <div v-for="star in 30" :key="star" class="star w-32 h-32 flex flex-col items-center cursor-pointer">
             <div
-              class="icon w-26 h-26 rounded-2xl backdrop-filter backdrop-blur-xl hover:scale-110 transition-all ease-in duration-200"
-            ></div>
+              class="icon w-26 h-26 rounded-2xl backdrop-filter backdrop-blur-xl hover:scale-110 transition-all ease-in duration-200">
+            </div>
             <div class="title text-neutral-100 text-center">star</div>
           </div>
           <div class="star w-32 h-32 flex flex-col items-center">
             <div
-              class="icon w-26 h-26 rounded-2xl backdrop-filter backdrop-blur-xl flex items-center justify-center text-3xl cursor-pointer"
-            >
+              class="icon w-26 h-26 rounded-2xl backdrop-filter backdrop-blur-xl flex items-center justify-center text-3xl cursor-pointer text-white">
               +
             </div>
           </div>
@@ -34,7 +25,8 @@
 
   <Dock class="mb-4" :model="items" position="bottom">
     <template #itemicon="{ item }">
-      <img v-tooltip.top="item.label" :src="item.icon" style="width: 100%" />
+      <img class="cursor-pointer" v-tooltip.top="item.label" :src="item.icon" style="width: 100%"
+        @click="openCorrespondingPage(item.label as string)" />
     </template>
   </Dock>
 </template>
@@ -46,24 +38,21 @@ import { Dock } from 'primevue'
 
 import 'swiper/css'
 import { ref } from 'vue'
+import animeIcon from '@/assets/bilibili.svg'
 
 const swiperRef = ref<SwiperType | null>(null)
 const items = ref([
   {
-    label: 'Finder',
-    icon: 'https://primefaces.org/cdn/primevue/images/dock/finder.svg',
-  },
-  {
-    label: 'App Store',
-    icon: 'https://primefaces.org/cdn/primevue/images/dock/appstore.svg',
-  },
-  {
-    label: 'Photos',
+    label: '未分类',
     icon: 'https://primefaces.org/cdn/primevue/images/dock/photos.svg',
   },
   {
-    label: 'Trash',
+    label: '老司机',
     icon: 'https://primefaces.org/cdn/primevue/images/dock/trash.png',
+  },
+  {
+    label: '动漫',
+    icon: animeIcon,
   },
 ])
 
@@ -74,6 +63,11 @@ const onSwiper = (swiper: SwiperType) => {
 const onSlideChange = () => {
   console.log('slide change')
 }
+
+const openCorrespondingPage = (label: string) => {
+  const idx = items.value.findIndex(item => item.label === label)
+  swiperRef.value?.slideTo(idx ?? 0)
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss"></style>
